6a3b69b1cb55e62018556bff974f55bcfc0b4a0d
Mason Raines <rainesmt92@gmail.com>
2019-01-14 20:32:34 -0500

    made basic layout to test media queries

---
+++ app.py
@@ -0,0 +1,33 @@
+
+
+from flask import Flask
+from flask import request
+# import requests
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    # Etsy: https://api.etsy.com/v2/listings/active.js?api_key=cdwxq4soa7q4zuavbtynj8wx&keywords=bicycle&includes=Images,Shop&sort_on=score
+
+    response = requests.get('https://swapi.co/api/planets/')
+    data = response.json()
+
+    planets = data['results']
+
+    planet_html = []
+
+    for planet in planets:
+        planet_html.append('<li>{} :: {}</li>'.format(planet['name'], planet['climate']))
+
+    planet_html = ''.join(planet_html)
+
+    # Get html and render to screen
+    index_file = open('index.html', 'r')
+    index_html = index_file.read()
+    index_html = index_html.replace('{{planet_list}}', planet_html)
+    index_file.close()
+
+    return index_html
+
